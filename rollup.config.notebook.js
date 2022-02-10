006a8db3d4b9ba954eea0d717b010640fe8b0edc
davidmunechika <davidam1203@gmail.com>
2022-02-09 22:13:36 -0500

    inject bundled js file

--- rollup.config.notebook.js
+++ rollup.config.notebook.js
@@ -1,11 +1,11 @@
 import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
 import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import image from '@rollup/plugin-image';
 import postcss from 'rollup-plugin-postcss';
+import json from '@rollup/plugin-json';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -15,28 +15,29 @@ export default {
     format: "iife",
     sourcemap: true,
   },
-    plugins: [
-        image(),
-        postcss({
-            extensions: [".css"],
-        }),
-        nodeResolve({
+  plugins: [
+    image(),
+    postcss({
+        extensions: [".css"],
+    }),
+    nodeResolve({
         extensions: [".js"],
-        }),
-        replace({
+    }),
+    replace({
+        preventAssignment: true,
         'process.env.NODE_ENV': JSON.stringify( 'development' )
-        }),
-        babel({
+    }),
+    babel({
         presets: ["@babel/preset-react"],
-        }),
-        commonjs(),
-        serve({
+    }),
+    commonjs(),
+    json(),
+    serve({
         open: true,
         verbose: true,
         contentBase: ["", "public"],
         host: "localhost",
         port: 3000,
-        }),
-        livereload({ watch: "dist" }),
-  ]
+    }),
+    ]
 };
